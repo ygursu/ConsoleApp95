48f803b26939df8f2f74ce566b30bcc9b9c87edf
ygursu <yasemingrs@gmail.com>
2024-02-26 22:21:37 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp95/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedengirilensayibolme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bolunen, bolen, bolum;
+
+            Console.WriteLine("Lütfen Bölünen sayıyı giriniz");
+            bolunen = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen Bölen sayıyı giriniz");
+            bolen = Int32.Parse(Console.ReadLine());
+
+            bolum = bolunen / bolen;
+
+            Console.WriteLine("Çıkan bölüm: {0}", bolum);
+            Console.ReadLine();
+
+
+        }
+    }
+}
